90704646a8a2315a0bd8727375d7bb55d3533bff
Henrik John <l.john@erasmusmc.nl>
2021-03-05 08:18:16 -0500

    Add updated smoking status covariate

---
+++ R/smokeCovariateCode.R
@@ -0,0 +1,135 @@
+# Copyright 2021 Observational Health Data Sciences and Informatics
+#
+# This file is part of SkeletonPredictionValidationStudy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#' Extracts smoker or ex-smoker status covariates
+#'
+#' @details
+#' This extracts smoker or ex-smoker from defined concept set ids
+#'
+#' @param connection  The database connection
+#' @param oracleTempSchema  The temp schema if using oracle
+#' @param cdmDatabaseSchema  The schema of the OMOP CDM data
+#' @param cdmVersion  version of the OMOP CDM data
+#' @param cohortTable  the table name that contains the target population cohort
+#' @param rowIdField  string representing the unique identifier in the target population cohort
+#' @param aggregated  whether the covariate should be aggregated
+#' @param cohortId  cohort id for the target population cohort
+#' @param covariateSettings  settings for the covariate cohorts and time periods
+#'
+#' @return
+#' The models will now be in the package
+#'
+#' @export
+getSmokingCovariateData <- function(connection,
+                                    oracleTempSchema = NULL,
+                                    cdmDatabaseSchema,
+                                    cdmVersion = "5",
+                                    cohortTable = "#cohort_person",
+                                    rowIdField = "row_id",
+                                    aggregated,
+                                    cohortId,
+                                    covariateSettings) {
+  
+  # Some SQL to construct the covariate:
+  sql <- paste("select lastdates.row_id, ",
+               "case when alldates.concept_id in (@smokercodes) then 1000+@analysisId",
+               " when alldates.concept_id in (@exsmokercodes) then 2000+@analysisId else 0 end as covariate_id, ",
+               "1 as value from ",
+               
+               "(select c.@row_id_field AS row_id, d.person_id, max(d.date) as date from ",
+               "(select person_id, condition_start_date as date from @cdm_database_schema.condition_occurrence where condition_concept_id in (@allCodes) union ",
+               "select person_id, procedure_date as date from @cdm_database_schema.procedure_occurrence where procedure_concept_id in (@allCodes) union ",
+               "select person_id, observation_date as date from @cdm_database_schema.observation where observation_concept_id in (@allCodes)) d",
+               "inner join @cohort_temp_table c on d.person_id = c.subject_id and d.date <= dateadd(day, @endDay, c.cohort_start_date) and d.date >= dateadd(day, @startDay, c.cohort_start_date) ",
+               "group by c.@row_id_field, d.person_id) lastdates ",
+               
+               "inner join ",
+               
+               "(select person_id, condition_concept_id as concept_id, condition_start_date as date from @cdm_database_schema.condition_occurrence where condition_concept_id in (@allCodes) union ",
+               "select person_id, procedure_concept_id as concept_id, procedure_date as date from @cdm_database_schema.procedure_occurrence where procedure_concept_id in (@allCodes) union ",
+               "select person_id, observation_concept_id as concept_id, observation_date as date from @cdm_database_schema.observation where observation_concept_id in (@allCodes)) alldates",
+               
+               " on lastdates.person_id = alldates.person_id and lastdates.date = alldates.date"
+  )
+  
+  sql <- SqlRender::render(sql,
+                           cohort_temp_table = cohortTable,
+                           row_id_field = rowIdField,
+                           startDay=covariateSettings$startDay,
+                           endDay=covariateSettings$endDay,
+                           smokercodes = paste(covariateSettings$smokerConceptSet, collapse = ','),
+                           exsmokercodes = paste(covariateSettings$exsmokerConceptSet, collapse = ','),
+                           allCodes = paste(c(covariateSettings$exsmokerConceptSet, covariateSettings$smokerConceptSet), collapse = ','),
+                           cdm_database_schema = cdmDatabaseSchema,
+                           analysisId = covariateSettings$analysisId
+  )
+  sql <- SqlRender::translate(sql, targetDialect = attr(connection, "dbms"),
+                              oracleTempSchema = oracleTempSchema)
+  # Retrieve the covariate:
+  covariates <- DatabaseConnector::querySql(connection, sql)
+  # Convert colum names to camelCase:
+  colnames(covariates) <- SqlRender::snakeCaseToCamelCase(colnames(covariates))
+  
+  
+  # Construct covariate reference:
+  covariateRef <- data.frame(covariateId = c(1000,2000)+covariateSettings$analysisId,
+                             covariateName = paste(c('Smoker during day','ex-smoker during day'),
+                                                   covariateSettings$startDay,
+                                                   'through',
+                                                   covariateSettings$endDay,
+                                                   'days relative to index:',
+                                                   covariateSettings$covariateName
+                             ),
+                             analysisId = rep(covariateSettings$analysisId,2),
+                             conceptId = rep(0,2))
+  
+  analysisRef <- data.frame(analysisId = covariateSettings$analysisId,
+                            analysisName = "smoker status covariate",
+                            domainId = "smoker status covariate",
+                            startDay = covariateSettings$startDay,
+                            endDay = covariateSettings$endDay,
+                            isBinary = "Y",
+                            missingMeansZero = "Y")
+  
+  metaData <- list(sql = sql, call = match.call())
+  result <- Andromeda::andromeda(covariates = covariates,
+                                 covariateRef = covariateRef,
+                                 analysisRef = analysisRef)
+  attr(result, "metaData") <- metaData
+  class(result) <- "CovariateData"	
+  return(result)
+}
+
+
+createSmokingCovariateSettings <- function(covariateName = 'Smoking status',
+                                           startDay=-1825, endDay=0, 
+                                           smokerConceptSet = c(4298794),
+                                           exsmokerConceptSet = c(4310250),
+                                           analysisId = 477
+) {
+  
+  covariateSettings <- list(covariateName=covariateName, 
+                            startDay=startDay,
+                            endDay=endDay,
+                            smokerConceptSet = smokerConceptSet,
+                            exsmokerConceptSet = exsmokerConceptSet,
+                            analysisId = analysisId
+  )
+  
+  attr(covariateSettings, "fun") <- "getSmokingCovariateData"
+  class(covariateSettings) <- "covariateSettings"
+  return(covariateSettings)
+}
--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -3,13 +3,14 @@ library(EmcWaltersDementiaModel)
 #=======================
 # The folder where the study intermediate and result files will be written:
 outputFolder <- "./EmcWaltersDementiaModelResults"
+options(andromedaTempFolder = "S:/temp/tempandromeda")
 
 # Details for connecting to the server:
-dbms <- "you dbms"
-user <- 'your username'
-pw <- 'your password'
-server <- 'your server'
-port <- 'your port'
+dbms <- "pdw"
+user <- NULL
+pw <- NULL
+server <- 'JRDUSAPSCTL01'
+port <- 17001
 
 connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 server = server,
@@ -18,52 +19,65 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 port = port)
 
 # Add the database containing the OMOP CDM data
-cdmDatabaseSchema <- 'cdm database schema'
+cdmDatabaseSchema <- c("CDM_Optum_Panther_v1355.dbo",
+                       "CDM_CPRD_v1299.dbo",
+                       "CDM_IBM_MDCR_v1352.dbo",
+                       "CDM_IQVIA_Germany_DA_v1376.dbo",
+                       "CDM_Optum_Extended_SES_v1387.dbo")
+
+cdmDatabaseName <- c('OPPANv1355',
+                     "CPRDv1299",
+                     "MDCRv1352",
+                     "IQGERv1376",
+                     "OPSESv1387")
+
 # Add a database with read/write access as this is where the cohorts will be generated
-cohortDatabaseSchema <- 'work database schema'
+cohortDatabaseSchema <- 'Scratch.dbo'
 
 oracleTempSchema <- NULL
 
 # table name where the cohorts will be generated
 cohortTable <- 'EmcWaltersDementiaModelCohort'
 
-# TAR settings
-sampleSize <- NULL
-riskWindowStart <- 1
-startAnchor <- 'cohort start'
-riskWindowEnd <- 365
-endAnchor <- 'cohort start'
-firstExposureOnly <- F
-removeSubjectsWithPriorOutcome <- F
-priorOutcomeLookback <- 99999
-requireTimeAtRisk <- F
-minTimeAtRisk <- 1
-includeAllOutcomes <- T
-
+# # TAR settings
+sampleSize <- 100000 #1000000
+# riskWindowStart <- 1
+# startAnchor <- 'cohort start'
+# riskWindowEnd <- 365*5
+# endAnchor <- 'cohort start'
+# firstExposureOnly <- F
+# removeSubjectsWithPriorOutcome <- F
+# priorOutcomeLookback <- 99999
+# requireTimeAtRisk <- T
+# minTimeAtRisk <- 365
+# includeAllOutcomes <- T
 
 #=======================
-
-EmcWaltersDementiaModel::execute(connectionDetails = connectionDetails,
-                                    cdmDatabaseSchema = cdmDatabaseSchema,
-                                    cdmDatabaseName = cdmDatabaseName,
-                                    cohortDatabaseSchema = cohortDatabaseSchema,
-                                    cohortTable = cohortTable,
-                                    sampleSize = sampleSize,
-                                    riskWindowStart = riskWindowStart,
-                                    startAnchor = startAnchor,
-                                    riskWindowEnd = riskWindowEnd,
-                                    endAnchor = endAnchor,
-                                    firstExposureOnly = firstExposureOnly,
-                                    removeSubjectsWithPriorOutcome = removeSubjectsWithPriorOutcome,
-                                    priorOutcomeLookback = priorOutcomeLookback,
-                                    requireTimeAtRisk = requireTimeAtRisk,
-                                    minTimeAtRisk = minTimeAtRisk,
-                                    includeAllOutcomes = includeAllOutcomes,
-                                    outputFolder = outputFolder,
-                                    createCohorts = T,
-                                    runAnalyses = T,
-                                    viewShiny = T,
-                                    packageResults = F,
-                                    minCellCount= 5,
-                                    verbosity = "INFO",
-                                    cdmVersion = 5)
+for (i in 4:length(cdmDatabaseName)) {
+  
+  EmcWaltersDementiaModel::execute(connectionDetails = connectionDetails,
+                                       cdmDatabaseSchema = cdmDatabaseSchema[i],
+                                       cdmDatabaseName = cdmDatabaseName[i],
+                                       cohortDatabaseSchema = cohortDatabaseSchema,
+                                       cohortTable = cohortTable,
+                                       sampleSize = sampleSize,
+                                       riskWindowStart = riskWindowStart,
+                                       startAnchor = startAnchor,
+                                       riskWindowEnd = riskWindowEnd,
+                                       endAnchor = endAnchor,
+                                       firstExposureOnly = firstExposureOnly,
+                                       removeSubjectsWithPriorOutcome = removeSubjectsWithPriorOutcome,
+                                       priorOutcomeLookback = priorOutcomeLookback,
+                                       requireTimeAtRisk = requireTimeAtRisk,
+                                       minTimeAtRisk = minTimeAtRisk,
+                                       includeAllOutcomes = includeAllOutcomes,
+                                       outputFolder = outputFolder,
+                                       createCohorts = T,
+                                       runAnalyses = T,
+                                       recalibrate = T,
+                                       viewShiny = F,
+                                       packageResults = F,
+                                       minCellCount= 5,
+                                       verbosity = "INFO",
+                                       cdmVersion = 5)
+}
