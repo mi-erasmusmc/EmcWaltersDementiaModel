d18e087f53cdfca473460614c6878d114157bbd5
Henrik John <l.john@erasmusmc.nl>
2021-03-18 08:58:27 -0400

    Update run file to default

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -3,14 +3,13 @@ library(EmcWaltersDementiaModel)
 #=======================
 # The folder where the study intermediate and result files will be written:
 outputFolder <- "./EmcWaltersDementiaModelResults"
-options(andromedaTempFolder = "S:/temp/tempandromeda")
 
 # Details for connecting to the server:
-dbms <- "pdw"
-user <- NULL
-pw <- NULL
-server <- 'JRDUSAPSCTL01'
-port <- 17001
+dbms <- "you dbms"
+user <- 'your username'
+pw <- 'your password'
+server <- 'your server'
+port <- 'your port'
 
 connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 server = server,
@@ -19,65 +18,85 @@ connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms,
                                                                 port = port)
 
 # Add the database containing the OMOP CDM data
-cdmDatabaseSchema <- c("CDM_Optum_Panther_v1355.dbo",
-                       "CDM_CPRD_v1299.dbo",
-                       "CDM_IBM_MDCR_v1352.dbo",
-                       "CDM_IQVIA_Germany_DA_v1376.dbo",
-                       "CDM_Optum_Extended_SES_v1387.dbo")
-
-cdmDatabaseName <- c('OPPANv1355',
-                     "CPRDv1299",
-                     "MDCRv1352",
-                     "IQGERv1376",
-                     "OPSESv1387")
-
+cdmDatabaseSchema <- 'cdm database schema'
 # Add a database with read/write access as this is where the cohorts will be generated
-cohortDatabaseSchema <- 'Scratch.dbo'
+cohortDatabaseSchema <- 'work database schema'
+# Add the database name
+cdmDatabaseName <- 'friendly database name'
 
 oracleTempSchema <- NULL
 
 # table name where the cohorts will be generated
 cohortTable <- 'EmcWaltersDementiaModelCohort'
 
-# # TAR settings
-sampleSize <- 100000 #1000000
-# riskWindowStart <- 1
-# startAnchor <- 'cohort start'
-# riskWindowEnd <- 365*5
-# endAnchor <- 'cohort start'
-# firstExposureOnly <- F
-# removeSubjectsWithPriorOutcome <- F
-# priorOutcomeLookback <- 99999
-# requireTimeAtRisk <- T
-# minTimeAtRisk <- 365
-# includeAllOutcomes <- T
+#=== Do not edit the following settings of the CodeToRun.R file 
+
+# TAR settings (will be ignored, refer to the study protocol for final settings)
+sampleSize <- NULL
+riskWindowStart <- 1
+startAnchor <- 'cohort start'
+riskWindowEnd <- 365
+endAnchor <- 'cohort start'
+firstExposureOnly <- F
+removeSubjectsWithPriorOutcome <- F
+priorOutcomeLookback <- 99999
+requireTimeAtRisk <- F
+minTimeAtRisk <- 1
+includeAllOutcomes <- T
+
 
 #=======================
-for (i in 4:length(cdmDatabaseName)) {
-  
-  EmcWaltersDementiaModel::execute(connectionDetails = connectionDetails,
-                                       cdmDatabaseSchema = cdmDatabaseSchema[i],
-                                       cdmDatabaseName = cdmDatabaseName[i],
-                                       cohortDatabaseSchema = cohortDatabaseSchema,
-                                       cohortTable = cohortTable,
-                                       sampleSize = sampleSize,
-                                       riskWindowStart = riskWindowStart,
-                                       startAnchor = startAnchor,
-                                       riskWindowEnd = riskWindowEnd,
-                                       endAnchor = endAnchor,
-                                       firstExposureOnly = firstExposureOnly,
-                                       removeSubjectsWithPriorOutcome = removeSubjectsWithPriorOutcome,
-                                       priorOutcomeLookback = priorOutcomeLookback,
-                                       requireTimeAtRisk = requireTimeAtRisk,
-                                       minTimeAtRisk = minTimeAtRisk,
-                                       includeAllOutcomes = includeAllOutcomes,
-                                       outputFolder = outputFolder,
-                                       createCohorts = T,
-                                       runAnalyses = T,
-                                       recalibrate = T,
-                                       viewShiny = F,
-                                       packageResults = F,
-                                       minCellCount= 5,
-                                       verbosity = "INFO",
-                                       cdmVersion = 5)
-}
+
+EmcWaltersDementiaModel::execute(connectionDetails = connectionDetails,
+                              cdmDatabaseSchema = cdmDatabaseSchema,
+                              cdmDatabaseName = cdmDatabaseName,
+                              cohortDatabaseSchema = cohortDatabaseSchema,
+                              cohortTable = cohortTable,
+                              sampleSize = sampleSize,
+                              riskWindowStart = riskWindowStart,
+                              startAnchor = startAnchor,
+                              riskWindowEnd = riskWindowEnd,
+                              endAnchor = endAnchor,
+                              firstExposureOnly = firstExposureOnly,
+                              removeSubjectsWithPriorOutcome = removeSubjectsWithPriorOutcome,
+                              priorOutcomeLookback = priorOutcomeLookback,
+                              requireTimeAtRisk = requireTimeAtRisk,
+                              minTimeAtRisk = minTimeAtRisk,
+                              includeAllOutcomes = includeAllOutcomes,
+                              outputFolder = outputFolder,
+                              createCohorts = T,
+                              runAnalyses = T,
+                              recalibrate = F,
+                              viewShiny = T,
+                              packageResults = T,
+                              minCellCount= 5,
+                              verbosity = "INFO",
+                              cdmVersion = 5)
+
+cdmDatabaseNameRecalibrated <- paste0(cdmDatabaseName, "_recalibrated")
+
+EmcWaltersDementiaModel::execute(connectionDetails = connectionDetails,
+                              cdmDatabaseSchema = cdmDatabaseSchema,
+                              cdmDatabaseName = cdmDatabaseNameRecalibrated,
+                              cohortDatabaseSchema = cohortDatabaseSchema,
+                              cohortTable = cohortTable,
+                              sampleSize = sampleSize,
+                              riskWindowStart = riskWindowStart,
+                              startAnchor = startAnchor,
+                              riskWindowEnd = riskWindowEnd,
+                              endAnchor = endAnchor,
+                              firstExposureOnly = firstExposureOnly,
+                              removeSubjectsWithPriorOutcome = removeSubjectsWithPriorOutcome,
+                              priorOutcomeLookback = priorOutcomeLookback,
+                              requireTimeAtRisk = requireTimeAtRisk,
+                              minTimeAtRisk = minTimeAtRisk,
+                              includeAllOutcomes = includeAllOutcomes,
+                              outputFolder = outputFolder,
+                              createCohorts = T,
+                              runAnalyses = T,
+                              recalibrate = T,
+                              viewShiny = T,
+                              packageResults = T,
+                              minCellCount= 5,
+                              verbosity = "INFO",
+                              cdmVersion = 5)
